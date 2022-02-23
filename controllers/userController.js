0c533686d5fc4c5ec2f7ba0ac569731a852c7f63
Abhishek Verma <abhishekvermamig21@gmail.com>
2022-02-23 15:13:10 +0530

    User Reg with password encrypt and JWT Tooken Added

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,8 +15,45 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
       url: "ProfilePicUrl",
     },
   });
+
+  const token = user.getJWTToken();
+
   res.status(200).json({
-      success: true,
-      user,
+    success: true,
+    user,
+    token,
   });
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // checking if user has given password and email both
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  console.log(user);
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password",401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password",401));
+  }
+
+  const token = user.getJWTToken();
+
+  res.status(200).json({
+    success: true,
+    user,
+    token,
+  });
+  
 });
