8085051cc485b1ec863a61ff5aad382005abadea
Abhishek Verma <abhishekvermamig21@gmail.com>
2022-02-23 21:44:36 +0530

    logeed User Passowrd Update done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const {registerUser,loginUser,logout,forgotPassword,resetPassword} = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  logout,
+  forgotPassword,
+  resetPassword,
+  getUserDetails,
+  updatePassword
+} = require("../controllers/userController");
 
-const router= express.Router();
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+
+const router = express.Router();
 
 router.route("/register").post(registerUser);
 
@@ -11,6 +22,9 @@ router.route("/password/forgot").post(forgotPassword);
 
 router.route("/password/reset/:token").put(resetPassword);
 
-
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser,getUserDetails);
+
+router.route("/password/update").put(isAuthenticatedUser,updatePassword)
 module.exports = router;
